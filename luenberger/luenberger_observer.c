a30274a35e6b5714730b488186224b7b2c120b20
fbaldo98 <francesc.baldisserr2@studio.unibo.it>
2019-05-06 12:55:11 +0200

    Modified Luenberger Matrices initialization with different sizes

--- luenberger/luenberger_observer.c
+++ luenberger/luenberger_observer.c
@@ -5,7 +5,7 @@ void InitLuembergerMatrices(luenberger_matrices_t * self)
 	// TODO
 	// Matrices size as input
 	InitMatrix(self->A, 6, 6, 0.0);
-	InitMatrix(self->B, 1, 6, 0.0);
-	InitMatrix(self->C, 6, 2, 0.0);
-	InitMatrix(self->L, 2, 6, 0.0);
+	InitMatrix(self->B, 6, 1, 0.0);
+	InitMatrix(self->C, 2, 6, 0.0);
+	InitMatrix(self->L, 6, 2, 0.0);
 }
