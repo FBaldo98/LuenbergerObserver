aedb04d23b7c1f880ea3ef669e3f984bffa49c42
fbaldo98 <francesc.baldisserr2@studio.unibo.it>
2019-05-06 13:49:30 +0200

    Added data matrices to observer

--- luenberger/luenberger_observer.c
+++ luenberger/luenberger_observer.c
@@ -8,4 +8,8 @@ void InitLuembergerMatrices(luenberger_matrices_t * self)
 	InitMatrix(self->B, 6, 1, 0.0);
 	InitMatrix(self->C, 2, 6, 0.0);
 	InitMatrix(self->L, 6, 2, 0.0);
+
+	InitMatrix(u_prev, 1, 1, 0.0);
+	InitMatrix(x_prev, 6, 1, 0.0);
+	InitMatrix(y_prev, 2, 1, 0.0);
 }
