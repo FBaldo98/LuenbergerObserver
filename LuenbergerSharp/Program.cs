cb89d1c88c91100c6f0f353fe2f9eba2fcbf528b
fbaldo98 <francesc.baldisserr2@studio.unibo.it>
2019-05-13 12:48:53 +0200

    LuenbergerSharp

---
+++ LuenbergerSharp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace LuenbergerSharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Matrix<double> A = DenseMatrix.OfArray(new double[,] {
+				{0, 0, 0, 0, 0, 0},
+				{-2000, -1.2, 0, 0, 20, 0},
+				{0, 0, 0, 1.0, 0, 0},
+				{163.9341, 0, -163.9341, 0, 0, -1.6393},
+				{0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0}});
+			Matrix<double> B = DenseMatrix.OfArray(new double[,]
+			{
+				{0},
+				{0},
+				{0},
+				{27.8688},
+				{0},
+				{0}});
+			Matrix<double> C = DenseMatrix.OfArray(new double[,] {
+				{100, 0, -100, 0, 0, 0},
+				{0, 0, 0, 17, 0, 0}
+			});
+			Matrix<double> L = DenseMatrix.OfArray(new double[,] {
+				{1.0, 10.0},
+				{51.0, 0541},
+				{0, 13},
+				{24, 870},
+				{59, 489},
+				{-324, -10508},
+			});
+
+			Matrix<double> U = DenseMatrix.OfArray(new double[,] { {0.5} });
+			Matrix<double> Y = DenseMatrix.OfArray(new double[,] { {0.7}, {0} });
+			Matrix<double> X = DenseMatrix.OfArray(new double[,] {
+				{0},
+				{0},
+				{0},
+				{0},
+				{0},
+				{0}});
+			Matrix<double> X_prev = DenseMatrix.OfArray(new double[,] {
+				{0},
+				{0},
+				{0},
+				{0},
+				{0},
+				{0}});
+
+			LuenbergerObserver(U, Y, X_prev, A, B, C, L);
+
+		}
+
+		public static void LuenbergerObserver(Matrix<double> U, Matrix<double> Y, Matrix<double> X_prev,Matrix<double> A, Matrix<double> B, Matrix<double> C, Matrix<double> L)
+		{
+			var res = A * X_prev + B * U + L * (Y - (C * X_prev));
+		}
+	}
+}
--- README.md
+++
@@ -1,5 +0,0 @@
-# LuenbergerObserver
-
-[![Build Status](https://travis-ci.com/FBaldo98/LuenbergerObserver.svg?token=nTBbtEGkmdyxznonbaKf&branch=master)](https://travis-ci.com/FBaldo98/LuenbergerObserver)
-
-Luenberger Observer implementation in C
